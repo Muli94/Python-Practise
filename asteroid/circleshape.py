470ca4b175864d96587ab49ac231f0236c88e969
Karol Mularczyk <karol.mularczyk@qodeca.com>
2025-05-15 13:23:55 +0200

    Finish asteroid game.

---
+++ asteroid/circleshape.py
@@ -0,0 +1,25 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, 'containers'):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, scree):
+        pass
+
+    def update(self):
+        pass
+
+    def collision(self, second_circle):
+        radius_sum = self.radius + second_circle.radius
+        distance = self.position.distance_to(second_circle.position)
+
+        return radius_sum > distance
+
